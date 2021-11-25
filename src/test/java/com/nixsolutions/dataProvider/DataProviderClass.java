814ddbd1d1e5bc2d8220a6606647b1a98c3fcae1
vitaliikucherenko <kucherenko.vitalik@gmail.com>
2021-11-25 18:29:47 +0200

    final project

--- src/test/java/com/nixsolutions/dataProvider/DataProviderClass.java
+++ src/test/java/com/nixsolutions/dataProvider/DataProviderClass.java
@@ -10,57 +10,57 @@ public class DataProviderClass {
 
     @DataProvider(name = "data-provider-new-user")
     public static Object[][] dataProviderNewUser() {
-        return new Object[][]{{"testvk1@mail.ru", "VkFirst1","2"}, {"testvk2@mail.ru", "Vk Last1","2"}};
+        return new Object[][]{{"testvk11@mail.ru", "VkFirst11","2"}, {"testvk21@mail.ru", "Vk Last11","2"}};
     }
 
     @DataProvider(name="data-provider-exist-user-name")
     public static Object[][] dataProviderExistUserName(){
-        return new Object[][]{{"newemail@mail.ru", "VkFirst1","2"}};
+        return new Object[][]{{"newemail@mail.ru", "VkFirst11","2"}};
     }
 
     @DataProvider(name="data-provider-exist-user-email")
     public static Object[][] dataProviderExistUserEmail(){
-        return new Object[][]{{"testvk2@mail.ru", "newName","2"}};
+        return new Object[][]{{"testvk21@mail.ru", "newName","2"}};
     }
 
     @DataProvider(name = "data-provider-login-user")
     public static Object[][] dataProviderLoginUser() {
-        return new Object[][]{{"testvk1@mail.ru", "2"}, {"testvk2@mail.ru", "2"}};
+        return new Object[][]{{"testvk11@mail.ru", "2"}, {"testvk21@mail.ru", "2"}};
     }
 
     @DataProvider(name = "data-provider-incorrect-login-user")
     public static Object[][] dataProviderIncorrectLoginUser() {
-        return new Object[][]{{"testvk2@mail.ru", "wrong"}, {"testvk2_wrong@mail.ru", "2"}};
+        return new Object[][]{{"testvk21@mail.ru", "wrong"}, {"testvk2_wrong@mail.ru", "2"}};
     }
 
     @DataProvider(name = "data-provider-user-with-task")
     public static Object[][] dataProviderCreateUserWithTask() {
-        return new Object[][]{{"testvk5@mail.ru", "First Last name4", "First task", "description",
+        return new Object[][]{{"testvk51@mail.ru", "First Last name41", "First task", "description",
                 "Second task", "description","19","20","hobby","address","name1","surname1",
                 "fathername1","cat","dog","parrot","cavy","hamster","squirrel","333 33 33","123456789012","m","04.10.1988","10.10.2020"}};
     }
 
     @DataProvider(name = "data-provider-creation-company")
     public static Object[][] dataProviderCreateCompany() {
-        return new Object[][]{{"Company 1", "ООО","testvk2@mail.ru","testvk5@mail.ru","testvk1@mail.ru"},
-                {"Company 2", "ООО","testvk2@mail.ru","testvk3@mail.ru","testvk1@mail.ru"}};
+        return new Object[][]{{"Company 1", "ООО","testvk21@mail.ru","testvk51@mail.ru","testvk11@mail.ru"},
+                {"Company 2", "ООО","testvk21@mail.ru","testvk51@mail.ru","testvk11@mail.ru"}};
     }
 
     @DataProvider(name = "data-provider-creation-company-wrong-owner")
     public static Object[][] dataProviderCreateCompanyWrongOwner() {
-        return new Object[][]{{"Company 3", "ООО","testvk2@mail.ru","testvk5@mail.ru","testvk1_wrong@mail.ru"},
-                {"Company 4", "ООО","testvk2@mail.ru","testvk3@mail.ru","testvk1_wrong@mail.ru"}};
+        return new Object[][]{{"Company 3", "ООО","testvk21@mail.ru","testvk51@mail.ru","testvk1_wrong@mail.ru"},
+                {"Company 4", "ООО","testvk21@mail.ru","testvk51@mail.ru","testvk1_wrong@mail.ru"}};
     }
 
     @DataProvider(name = "data-provider-creation-company-without-users")
     public static Object[][] dataProviderCreateCompanyWithoutUsers() {
-        return new Object[][]{{"Company 3", "ООО","","testvk3_notexist@mail.ru","testvk1@mail.ru"},
-                {"Company 4", "ООО","testvk2_notexist@mail.ru","","testvk1@mail.ru"}};
+        return new Object[][]{{"Company 3", "ООО","","testvk3_notexist@mail.ru","testvk11@mail.ru"},
+                {"Company 4", "ООО","testvk2_notexist@mail.ru","","testvk11@mail.ru"}};
     }
 
     @DataProvider(name = "data-provider-creation-company-with-wrong-type")
     public static Object[][] dataProviderCreateCompanyWithWrongType() {
-        return new Object[][]{{"Company 1", "ОAО", "testvk2@mail.ru", "testvk5@mail.ru", "testvk1@mail.ru"},
-                {"Company 2", "TOB", "testvk2@mail.ru", "testvk5@mail.ru", "testvk1@mail.ru"}};
+        return new Object[][]{{"Company 1", "ОAО", "testvk21@mail.ru", "testvk51@mail.ru", "testvk11@mail.ru"},
+                {"Company 2", "TOB", "testvk21@mail.ru", "testvk51@mail.ru", "testvk11@mail.ru"}};
     }
 }
